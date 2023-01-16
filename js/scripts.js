f955c270ceaf5e6eadb05bbd5262efc064acb315
Brandon Vasquez <thatsjaskett@gmail.com>
2023-01-16 15:14:31 +0100

    Modifica dell'array

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,32 @@
 const eleCardsContainer = document.querySelector('.cards-container');
 
-const persona1 = {
-    'name': 'Wayne Barnett',
-    'role': 'Founder & CEO',
-    'image': 'wayne-barnett-founder-ceo.jpg'
-};
-
-const persona2 = {
-    'name': 'Angela Caroll',
-    'role': 'Chief Editor',
-    'image': 'angela-caroll-chief-editor.jpg'
-};
-
-const persona3 = {
-    'name': 'Walter Gordon',
-    'role': 'Office Manager',
-    'image': 'walter-gordon-office-manager.jpg'
-};
-
-const persona4 = {
-    'name': 'Angela Lopez',
-    'role': 'Social Media Manager',
-    'image': 'angela-lopez-social-media-manager.jpg'
-};
-
-const persona5 = {
-    'name': 'Scott Estrada',
-    'role': 'Developer',
-    'image': 'scott-estrada-developer.jpg'
-};
-
-const persona6 = {
-    'name': 'Barbara Ramos',
-    'role': 'Graphic Designer',
-    'image': 'barbara-ramos-graphic-designer.jpg'
-};
-
-const arrEmployers = [persona1, persona2, persona3, persona4, persona5, persona6];
-
-// console.log(arrEmployers);
+const arrEmployers = [
+    {
+        'name': 'Wayne Barnett',
+        'role': 'Founder & CEO',
+        'image': 'wayne-barnett-founder-ceo.jpg'
+    }, {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'image': 'angela-caroll-chief-editor.jpg'
+    }, {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'image': 'walter-gordon-office-manager.jpg'
+    }, {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'image': 'angela-lopez-social-media-manager.jpg'
+    }, {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'image': 'scott-estrada-developer.jpg'
+    }, {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'image': 'barbara-ramos-graphic-designer.jpg'
+    }
+];
 
 printCards();
 
@@ -63,5 +51,7 @@ function printCards() {
         const newP = document.createElement('p');
         newP.innerHTML = arrEmployers[i].role;
         newDiv.append(newP);
+
+        console.log(arrEmployers[i].name);
     }
 }
