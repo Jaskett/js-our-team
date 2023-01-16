342e92e934f2a6ad2b6190bf5b8daa65356a49e6
Brandon Vasquez <thatsjaskett@gmail.com>
2023-01-16 14:49:59 +0100

    Milestone 0

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+const persona1 = {
+    'name': 'Wayne Barnett',
+    'role': 'Founder & CEO',
+    'image': 'wayne-barnett-founder-ceo.jpg'
+};
+
+const persona2 = {
+    'name': 'Angela Caroll',
+    'role': 'Chief Editor',
+    'image': 'angela-caroll-chief-editor.jpg'
+};
+
+const persona3 = {
+    'name': 'Walter Gordon',
+    'role': 'Office Manager',
+    'image': 'walter-gordon-office-manager.jpg'
+};
+
+const persona4 = {
+    'name': 'Angela Lopez',
+    'role': 'Social Media Manager',
+    'image': 'angela-lopez-social-media-manager.jpg'
+};
+
+const persona5 = {
+    'name': 'Scott Estrada',
+    'role': 'Developer',
+    'image': 'scott-estrada-developer.jpg'
+};
+
+const persona6 = {
+    'name': 'Barbara Ramos',
+    'role': 'Graphic Designer',
+    'image': 'barbara-ramos-graphic-designer.jpg'
+};
+
+const arrEmployers = [persona1, persona2, persona3, persona4, persona5, persona6];
+
+console.log(arrEmployers);
